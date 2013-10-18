ccbe0cae7ff0085714a767765b74570e29005d0f
Ruzbeh Bacha <ruzbehbacha@gmail.com>
2013-10-18 12:34:34 +0100

    completed airport tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,20 +1,28 @@
 class Airport
 
+	CAPACITY = 20
+
 	def initialize
 		@planes = []
+		@capacity = CAPACITY
 
 	end
 
 	def plane_count
-		@planes.count
+		@planes.length
 	end
 
 	def land plane
+		return 'Sorry the airport is full, you can\'t land' if full?
 		@planes << plane
 	end
 
 	def take_off plane
+		@planes.delete(plane)
+	end
 
+	def full?
+		plane_count == @capacity
 	end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,26 +6,42 @@ require 'airport'
 # It is up to you how many planes can land in the airport and how that is impermented.
 #
 # If the airport is full then no planes can land
+
 describe Airport do
   let(:airport) { Airport.new }
   let(:plane) {double :Plane}
+  CAPACITY = 20
   
+  def fill_airport_with plane
+    CAPACITY.times {airport.land(plane)}
+  end
+
   context 'taking off and landing' do
+    
     it 'a plane can land' do
-    expect(airport.plane_count).to eq 0
-    airport.land(plane)
-    expect(airport.plane_count).to eq 1
+      expect(airport.plane_count).to eq 0
+      airport.land plane
+      expect(airport.plane_count).to eq 1
     end
     
     it 'a plane can take off' do
-    airport.land(plane)
-    airport.take_off(plane)
-    expect(airport.plane_count).to eq 0
+      airport.land(plane)
+      airport.take_off plane
+      expect(airport.plane_count).to eq 0
     end
   end
   
   context 'traffic control' do
+    
+    it 'should know when the airport is full' do
+      expect(airport).not_to be_full
+      fill_airport_with plane
+      expect(airport).to be_full
+    end
+
     it 'a plane cannot land if the airport is full' do
+      fill_airport_with plane
+      expect(airport.land plane).to eq 'Sorry the airport is full, you can\'t land'
     end
     
     # Include a weather condition using a module.
